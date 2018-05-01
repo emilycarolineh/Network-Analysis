26d25ac6b9e1d5d8b5768f63cc2c1eec7adcf5ad
Emily Higgs <emilycarolineh@gmail.com>
2018-04-30 18:08:05 -0700

    A class that handles union find operations, supporting K
    rvtvre
    berbwrtbw
    t
    q3

---
+++ UnionFind.java
@@ -0,0 +1,48 @@
+public class UnionFind {
+
+	private int numberOfSubsets;
+	private int[] id;
+	private int[] sz;
+
+
+	public UnionFind(int vertices) {
+
+		numberOfSubsets = vertices;
+		id = new int[vertices];
+		sz = new int[vertices];
+		for (int i = 0; i < vertices; i++) { 
+			id[i] = i; 
+			sz[i] = 1; 
+		}
+	}
+
+	public int getSubsetNumber() {
+		return numberOfSubsets;
+	}
+
+	public void union(int p, int q) {
+		int i = find(p);
+		int j = find(q);
+
+		if (i == j) 
+			return;
+
+		if (sz[i] < sz[j]) { 
+			id[i] = j; 
+			sz[j] += sz[i]; 
+		}
+		else { 
+			id[j] = i; 
+			sz[i] += sz[j]; 
+		}
+
+		numberOfSubsets--;
+	}
+
+	public int find(int p) {
+		while (p != id[p]) 
+			p = id[p];
+
+		return p;
+	}
+}
